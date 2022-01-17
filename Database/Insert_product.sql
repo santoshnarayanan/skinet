8520a364c335c7c0395ef2adc4ca427de99b16c3
DESKTOP-Q2DMJDO\santosh narayanan <santosh33@gmail.com>
2022-01-17 17:27:36 +0530

    Add project files.

---
+++ Database/Insert_product.sql
@@ -0,0 +1,16 @@
+﻿-- Script Date: 1/17/2022 4:02 PM  - ErikEJ.SqlCeScripting version 3.5.2.90
+INSERT INTO [Products]
+           ([Id]
+           ,[Name])
+     VALUES (1,'Product one');
+
+INSERT INTO [Products]
+           ([Id]
+           ,[Name])
+     VALUES (2,'Product two');
+
+
+INSERT INTO [Products]
+           ([Id]
+           ,[Name])
+     VALUES (3,'Product three');
