df7eb66788a2a705c3773998fa73adf9f3abb1fb
DESKTOP-Q2DMJDO\santosh narayanan <santosh33@gmail.com>
2022-01-26 15:12:06 +0530

    Chapter3-Completed

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,5 @@
-﻿using Core.Entites;
+﻿using System.Reflection;
+using Core.Entites;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
@@ -10,5 +11,15 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
